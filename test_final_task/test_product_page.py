cfba407d85aa7003badc448f2d3681a62b6d5ada
Elena7788 <sheremet_e@ukr.net>
2021-03-03 15:51:04 +0200

    content independence, looking for a bug

--- test_final_task/test_product_page.py
+++ test_final_task/test_product_page.py
@@ -1,15 +1,28 @@
 import time
 
+import pytest
+
 from .pages.product_page import ProductPage
 
-
+'''
 def test_guest_can_add_product_to_basket(browser):
     # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+'''
+
+'''
+@pytest.mark.parametrize('promo_offer', ["0","1", "3", "4", "5", "6", "7", "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+'''
+
+@pytest.mark.parametrize('promo_offer', [*range(1,7), pytest.param(7, marks=pytest.mark.xfail), *range(8,10)])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}'
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
-    time.sleep(5)
+    time.sleep(10)
     page.should_be_product_name_in_basket()
     page.should_be_product_price_in_basket()
